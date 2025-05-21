cd0252a894e248b469a0b4acd0705257dd5288f5
Maksym Prokopov <maksym.prokopov@billie.io>
2025-05-21 09:32:30 +0200

    test: introduce the error

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-    return Simple{"Hello", "Dear Students", host}
+    return Simple{"Hello", "Dear Students", "something else"}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
