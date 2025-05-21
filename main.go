94308723751df5cf870ba98fd8c0b0823c241a95
Maksym Prokopov <maksym.prokopov@billie.io>
2025-05-21 09:28:20 +0200

    feat: introduce unit testing

--- main.go
+++ main.go
@@ -13,8 +13,13 @@ type Simple struct {
     Url         string
 }
 
+func SimpleFactory(host string) Simple {
+    return Simple{"Hello", "Dear Students", host}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    simple := Simple{"Hello", "Dear Students", r.Host}
+    // simple := Simple{"Hello", "Dear Students", r.Host}
+    simple := SimpleFactory(r.Host)
 
     jsonOutput, _ := json.Marshal(simple)
 
